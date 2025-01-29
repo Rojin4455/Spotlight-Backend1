0752da34743234e1675f6da1abec50bd88e19c97
Rojin Saji <rojinsaji@Rojins-MacBook-Air.local>
2025-01-29 10:52:08 +0530

    added aws hostname in allowed hosts

--- spotlightbackend/settings.py
+++ spotlightbackend/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '3.110.46.215']
 
 
 # Application definition
