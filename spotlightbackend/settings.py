6c40139484eaa53cda09007de2b67d158c1f8ee9
Rojin Saji <rojinsaji@Rojins-MacBook-Air.local>
2025-01-29 12:32:33 +0530

    added static and media

--- spotlightbackend/settings.py
+++ spotlightbackend/settings.py
@@ -16,6 +16,7 @@ from datetime import timedelta
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import os
 
 
 
@@ -193,6 +194,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+# Media files settings
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
